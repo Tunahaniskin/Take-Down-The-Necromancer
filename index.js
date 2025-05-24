453bc35ee454cd554b81d3d4c774afa4116bd446
Tunahaniskin <146743359+Tunahaniskin@users.noreply.github.com>
2025-05-24 08:52:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+import { Player } from "./player.js";
+import { Enemy } from "./enemy.js";
+import { Cards } from "./cards.js";
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resizeCanvas();
+
+const player = new Player();
+const enemy = new Enemy();
+const cards = new Cards();
+
+
+cards.generateCardsBarIcon();
+cards.resetPlayerCardsForNewRound();
+  
+ 
+
+function drawAll(timestamp) {
+
+  player.drawPlayer(ctx);
+  player.drawHealthBar(ctx);
+  player.drawHealthIcon(ctx, timestamp);
+  
+
+  enemy.drawEnemy(ctx);
+  enemy.drawHealthBar(ctx);
+  enemy.drawHealthIcon(ctx,timestamp);
+  enemy.drawComboStatus(ctx);
+
+  
+
+  cards.drawPlayerCardsBar(ctx, player.x, player.y);  
+  cards.drawCardsBar(ctx);
+  cards.drawEnemyCardsBar(ctx, enemy.x, enemy.y);
+  cards.drawCardsBarIcon(ctx); 
+  
+  
+
+  requestAnimationFrame(drawAll);
+}
+
+player.HealthIcon.onload = () => {
+  requestAnimationFrame(drawAll);
+};
+
